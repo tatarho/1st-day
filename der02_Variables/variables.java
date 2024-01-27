da6e06a2aaf05419c1649fad898ba0f23945a894
Seyfettin AZAKLI <157910337+tatarho@users.noreply.github.com>
2024-01-27 23:08:34 +0300

    Add files via upload

---
+++ der02_Variables/variables.java
@@ -0,0 +1,14 @@
+package der02_Variables;
+
+public class variables {
+    public static void main(String[]args){
+
+        int sayi=15;//declearation(int) bir kere olu ama asignment çok olabilir.
+        System.out.println(sayi);
+
+        sayi=25;System.out.println(sayi);
+        sayi=sayi+10;System.out.println(sayi);
+        //bir asignment işareti görüldüğünde önce sağ tarafı yaparız ve enson değer kabul edilir.
+    }
+
+}
